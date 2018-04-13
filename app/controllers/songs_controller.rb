98083fd9fe725d146d8f832497d4fed8bc46fb0b
Hoon Lee <yl.hoony@gmail.com>
2018-04-13 20:29:31 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+  require 'csv'
 
   def index
     @songs = Song.all
@@ -45,10 +46,18 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
+  def upload
+    binding.pry
+    
+    # CSV.foreach(params[:songs].path, headers: true) do |song|
+    #   Song.create(title: song[0], artist_id: Artist.find_by(name: song[1]))
+    # end
+    redirect_to songs_path
+  end
+
   private
 
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
